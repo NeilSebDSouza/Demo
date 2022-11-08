1fc3f2825a146c14dd9430fc530c17988ace1cc1
NeilSebDSouza <117694655+NeilSebDSouza@users.noreply.github.com>
2022-11-07 16:52:12 -0800

    Add files via upload

---
+++ p5js_template-main/p5js_template-main/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 900);
+
+  // var sprite = createSprite(200, 200,40 , 90);
+  // sprite.shapeColor = "red"
+
+  console.log("hiiiiiiiiiiiiiiiiiiiii")
+}
+
+function draw(){
+
+    background("black");
+
+    // drawSprites()
+  
+}
